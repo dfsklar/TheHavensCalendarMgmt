8baaf0c0ab58f878de6f7419f9cc72f87cfb8545
root <root@li479-42.members.linode.com>
2016-09-10 13:52:31 -0400

    Documentation

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,19 @@
 # $1 = target name such as "bh" or "sh" or "union"
 # $2 = the URL to proxy
 
+# Why do this?
+
+# It turns out that many rental-marketplace services cannot 
+# read an ICS file directly from google's servers, but are
+# perfectly happy with a URL that points to my own server
+# EVEN THOUGH THE ICS CONTENT IS IDENTICAL.
+
+# So this really is nothing more than a copy from google's servers
+# to mine... with absolutely ZERO modification of the ICS content.
+
+# Care is taken to ensure the destination file is overwritten only
+# if the wget from google's servers produced a valid non-empty file.
+
 TARGET=$1
 URL=$2
 DEST_FOLDER=$3

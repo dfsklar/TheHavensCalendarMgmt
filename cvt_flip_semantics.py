990b265bc908216bfc2b3f9e1746de60c460d7b6
root <root@li479-42.members.linode.com>
2016-09-10 15:20:32 -0400

    working on deploy of the flip-format converter

--- cvt_flip_semantics.py
+++ cvt_flip_semantics.py
@@ -8,7 +8,7 @@ condo_unit = sys.argv[1]
 from dateutil.parser import parse
 
 for line in sys.stdin:
-    (field1, field2) = line.split(':')
+    (field1, field2) = line.split(':', 1)
     if field1 == 'DTSTART;VALUE=DATE':
         start_day = parse(field2).day
     elif field1 == "DTEND;VALUE=DATE":
@@ -18,14 +18,7 @@ for line in sys.stdin:
         print "%s:%s" % (field1, end_tomorrow.strftime("%Y%m%d"))
         continue
     elif field1 == "SUMMARY":
-        print "%s:%s %s-%s %s" % (field1, condo_unit, start_day, end_tomorrow_day, field2)
-        sys.exit(1)
-    else:
-        print line.rstrip()
+        print "%s:%s %s-%s %s" % (field1, condo_unit, start_day, end_tomorrow_day, field2.rstrip())
+        continue
 
-
-
-
-
-
-
+    print line.rstrip()
---
+++ refresh_all_force.sh
@@ -0,0 +1,22 @@
+cd /root/TheHavensCalendarMgmt/
+
+DF=/var/www/BIRCHHAVEN/goocal_proxy/
+
+echo "About to re-pull from SH goocal" && \
+sh refresh.sh sh https://calendar.google.com/calendar/ical/nu1je77d8je49j11rjfbd3tnjg%40group.calendar.google.com/private-a013b65e5ed16f0ca12971a9a6a2269c/basic.ics $DF && \
+echo "About to re-pull from BH goocal" && \
+sh refresh.sh bh https://calendar.google.com/calendar/ical/humh3m6csgsc1en0893s9ibfdc%40group.calendar.google.com/private-fc7d6846b717bcf2c8a9d454de55aa7a/basic.ics $DF && \
+echo "About to re-pull from UNION goocal" && \
+sh refresh.sh union https://calendar.google.com/calendar/ical/ruck03hfkh84jlb6l93fg0sob4%40group.calendar.google.com/private-35a3c6e9eaa7fbc02dc5e282db3d8e2e/basic.ics $DF && \
+echo "About to produce 'flip format' version for BH" && \
+sh cvt_flip_semantics.sh $DF/bh.ics $DF/bh_flipfmt.ics 404C && \
+echo "About to produce 'flip format' version for SH" && \
+sh cvt_flip_semantics.sh $DF/sh.ics $DF/sh_flipfmt.ics 402C
+
+if test $STAT -eq 0
+then
+    echo "OK status - fresh pulls from GooCals all done"
+else
+    echo "SOMETHING BAD HAPPENED"
+    echo $STAT
+fi

a3714ff0cff509d2c196e67565c256bd14bf7baa
root <root@li173-145.members.linode.com>
2019-02-01 06:00:07 -0500

    ready for testing

--- update_antisingleunit.py
+++ update_antisingleunit.py
@@ -1,12 +1,16 @@
 # For help:
 #    https://stackoverflow.com/questions/9044084/efficient-date-range-overlap-calculation-in-python
+#
+# TO TEST:  python update_antisingleunit.py < /var/www/BIRCHHAVEN/goocal_proxy/union.ics
 
 import sys
 import datetime
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from collections import namedtuple
 
-Range = namedtuple('Range', ['start', 'end'])
+Range = namedtuple('Range', ['startnight', 'lastnight'])
+
+ranges = []
 
 # r1 = Range(start=datetime(2012, 1, 15), end=datetime(2012, 5, 10))
 # r2 = Range(start=datetime(2012, 3, 20), end=datetime(2012, 9, 15))
@@ -25,23 +29,58 @@ for line in sys.stdin:
     if line[0] != ' ':
         (field1, field2) = line.split(':', 1)
         if field1 == 'DTSTART;VALUE=DATE':
-            start_day = parse(field2)
+            start_day = parse(field2).date()
         elif field1 == 'DTSTAMP':
             dtstamp = field2
         elif field1 == "DTEND;VALUE=DATE":
-            end_today = parse(field2)
-            end_tomorrow = end_today + timedelta(days=1)
-            end_tomorrow_day = end_tomorrow.day
-            # print "%s:%s" % (field1, end_tomorrow.strftime("%Y%m%d"))
+            checkout_date = parse(field2).date()
+            last_booked_date = checkout_date - timedelta(days=1)
         elif field1 == "SUMMARY":
             is_bothunit_booking = ('BOTH' in field2.strip())
             summary = field2
         elif field1 == "END" and ("VEVENT" in field2):
-            if not is_bothunit_booking:
-                print "-----------------"
-                print summary
-                print start_day
-                print end_today
-# TO TEST:  python update_antisingleunit.py < /var/www/BIRCHHAVEN/goocal_proxy/union.ics
+            if not is_bothunit_booking and (last_booked_date > date.today()):
+                ranges.append(Range(startnight=start_day, lastnight=last_booked_date))
+
+print ranges
+
+curdate = datetime(2015,1,1)
+
+print """BEGIN:VCALENDAR
+PRODID:-//Google Inc//Google Calendar 70.9054//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:HAVENS-AntiSingleUnit
+X-WR-TIMEZONE:America/Los_Angeles
+X-WR-CALDESC:Inverse of all single-unit bookings"""
 
 
+
+calentry = """BEGIN:VEVENT
+DTSTART;VALUE=DATE:%s
+DTEND;VALUE=DATE:%s
+DTSTAMP:20190201T103802Z
+UID:havensantisingleunit@google.com
+CREATED:20190201T103202Z
+DESCRIPTION:
+LAST-MODIFIED:20190201T103202Z
+LOCATION:
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:AntiSingleUnit Blocker
+TRANSP:OPAQUE
+END:VEVENT"""
+
+
+# TODO:  SORTING!
+for R in ranges:
+    inverse = Range(startnight=curdate, lastnight=R.startnight - timedelta(days=1))
+    print calentry % (inverse.startnight.strftime("%Y%m%d"), inverse.lastnight.strftime("%Y%m%d"))
+    curdate = R.lastnight + timedelta(days=1)
+
+
+print calentry % (curdate.strftime("%Y%m%d"), "2066-01-01")
+
+print "END:VCALENDAR"
+    

30b4c1657d727f931c565a733198ff6eb938296b
David F. Sklar <dfsklar@gmail.com>
2019-03-09 00:46:48 +0000

    DEPLOYED

--- update_antisingleunit.py
+++ update_antisingleunit.py
@@ -4,45 +4,88 @@
 # TO TEST:  python update_antisingleunit.py < /var/www/BIRCHHAVEN/goocal_proxy/union.ics
 
 import sys
-import datetime
-from datetime import datetime, timedelta, date
 from collections import namedtuple
+import arrow
 
 Range = namedtuple('Range', ['startnight', 'lastnight'])
-
-ranges = []
-
 # r1 = Range(start=datetime(2012, 1, 15), end=datetime(2012, 5, 10))
 # r2 = Range(start=datetime(2012, 3, 20), end=datetime(2012, 9, 15))
 
-# latest_start = max(r1.start, r2.start)
-# earliest_end = min(r1.end, r2.end)
-# delta = (earliest_end - latest_start).days + 1
-# overlap = max(0, delta)
+
+
+today = arrow.now().replace(hour=0, minute=0, second=0, microsecond=0)
+finaldate = today.shift(years=2)
+
+
+alldates = list(arrow.Arrow.range('day', today, finaldate))
+
+THECAL = {}
+
+def day_of_year(arrowdate):
+    return int(x.format('DDD'))
+
+def is_weekend(arrowdate):
+    # FRI and SAT are the weekends
+    return int(x.format('d')) in [5, 6]
+
+def printdebug(x):
+    print '---------------------'
+    print x
+    print x.format('dddd')
+    print day_of_year(x)
+    print "WEEKEND" if is_weekend(x) else "weekday"
+    print THECAL[x]
+
+
+def is_in_peak_period(arrowdate):
+    doy = day_of_year(arrowdate)
+    return (doy < 2) or (doy>=165 and doy<=257) or (doy>=350)
+
+
+# STEP 1: Block all dates as the default
+for x in alldates:
+    THECAL[x] = 'blocked'
+
+# STEP 2: Unblock all non-peak weekdays
+for x in alldates:
+    if not is_weekend(x):
+        if not is_in_peak_period(x):
+            THECAL[x] = 'avail'
 
 
 # ON STDIN: The UNION calendar ics file
-
-from dateutil.parser import parse
-
+# STEP 3: Unblock any days that are already booked to a single unit
 for line in sys.stdin:
     if line[0] != ' ':
         (field1, field2) = line.split(':', 1)
         if field1 == 'DTSTART;VALUE=DATE':
-            start_day = parse(field2).date()
+            start_day = arrow.get(field2)
         elif field1 == 'DTSTAMP':
             dtstamp = field2
         elif field1 == "DTEND;VALUE=DATE":
-            checkout_date = parse(field2).date()
-            last_booked_date = checkout_date - timedelta(days=1)
+            checkout_date = arrow.get(field2)
+            last_booked_date = checkout_date.shift(days=-1)
         elif field1 == "SUMMARY":
             is_bothunit_booking = ('BOTH' in field2.strip())
             summary = field2
         elif field1 == "END" and ("VEVENT" in field2):
-            if not is_bothunit_booking and (last_booked_date > date.today()):
-                ranges.append(Range(startnight=start_day, lastnight=last_booked_date))
+            if not is_bothunit_booking and (last_booked_date > today):
+                print "Processing " + line
+                while start_day <= last_booked_date:
+                    THECAL[start_day] = 'avail'
+                    start_day = start_day.shift(days=1)
 
-curdate = datetime(2015,1,1)
+# STEP 4: Unblock any days that are "imminent"
+for x in arrow.Arrow.range('day', today, today.shift(weeks=2)):
+    THECAL[x] = 'avail'
+for x in arrow.Arrow.range('day', today.shift(weeks=2), today.shift(weeks=3)):
+    if not is_in_peak_period(x):
+        THECAL[x] = 'avail'
+
+# DEBUG EMISSION
+#for x in alldates:
+#    printdebug(x)
+
 
 print """BEGIN:VCALENDAR
 PRODID:-//Google Inc//Google Calendar 70.9054//EN
@@ -71,19 +114,20 @@ TRANSP:OPAQUE
 END:VEVENT"""
 
 def print_calentry(date1, date2):
-    print calentry % (date1, date2, date1)
+    fmt = 'YYYYMMDD'
+    print calentry % (date1.format(fmt), date2.format(fmt), date1.format(fmt))
     
 
-# TODO:  SORTING!
-idx = 1
-for R in ranges:
-    inverse = Range(startnight=curdate, lastnight=R.startnight)
-    print_calentry(inverse.startnight.strftime("%Y%m%d"), inverse.lastnight.strftime("%Y%m%d"))
-    idx += 1
-    curdate = R.lastnight + timedelta(days=1)
-
-
-print_calentry(curdate.strftime("%Y%m%d"), "20660101")
-
+curblockstart = None
+for x in alldates:
+    if curblockstart:
+        if THECAL[x] == 'avail':
+            # We have found the end of a blocked range
+            print_calentry(curblockstart, x.shift(days=-1))
+            curblockstart = None
+    else:
+        if THECAL[x] == 'blocked':
+            curblockstart = x
+        
 print "END:VCALENDAR"
     
